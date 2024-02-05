411bbc40091e0bec8264396b3c3ab9b6850ecb09
Rasel Hossen <raselhossen052@gmail.com>
2024-02-05 20:55:09 +0600

    setup/shadcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between'>
+    <main className='flex items-center justify-between'>
       HELLO WORLD
+      <h1>button</h1>
+      <Button variant='link'>Link</Button>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,11 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        inter: ['var(--font-inter)'],
+        anekBangla: ['var(--font-anek-bangla)'],
+        notoSansBengali: ['var(--font-noto-sans-bengali)'],
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
